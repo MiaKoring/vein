d551952585c2d18f18f18d8b295c99da2184fd51
Mia Koring <koring.mia@gmail.com>
2025-10-26 04:30:46 +0100

    final query state (for today)
    reasonably performant with 560k items

--- Sources/BetterSyncSwiftUI/Query.swift
+++ Sources/BetterSyncSwiftUI/Query.swift
@@ -1,49 +1,15 @@
 import SwiftUI
 import BetterSync
-import Combine
+import os.log
 
-/*@propertyWrapper
-public struct Query<T: PersistentModel>: DynamicProperty {
-    @State private var results: [T] = []
-    
-    public init() {}
-    
-    public var wrappedValue: [T] {
-        results
-    }
-    
-    public func update() {
-        do {
-            results = try ManagedObjectContext.instance.fetchAll(T.self)
-        } catch {
-            print("Fetch error: \(error)")
-            results = []
-        }
-    }
-}*/
-/*
-@propertyWrapper
-public struct Query<T: PersistentModel>: DynamicProperty {
-    @State private var results: [T] = []
-    
-    public init() {}
-    
-    public var wrappedValue: [T] {
-        results
-    }
-    
-    public var projectedValue: Binding<[T]> {
-        $results
-    }
-}
-*/
-
+@available(macOS 14.0, *)
+@Observable
 @MainActor
 @propertyWrapper
 public class Query<M: PersistentModel>: DynamicProperty {
     public typealias WrappedType = [M]
     
-    @State private var cachedItems: [M]?
+    private var cachedItems: [M]?
     
     public var wrappedValue: [M] {
         if let cached = cachedItems {
@@ -63,4 +29,3 @@ public class Query<M: PersistentModel>: DynamicProperty {
     
     public init() { }
 }
-
